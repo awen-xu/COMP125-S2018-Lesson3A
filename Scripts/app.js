40c78f0010d46ee87aa5e46d60ba8365cd299fdc
awen-xu <wxu59@my.centennialcollege.ca>
2018-05-23 12:13:41 -0400

    end of class 2 of week 3

--- Scripts/app.js
+++ Scripts/app.js
@@ -67,6 +67,90 @@ let app = (function () {
         let content = document.getElementsByClassName("content");
         console.log(myArray);
         console.log("myArray length: " + myArray.length);
+
+        /* // loop type 1 - classic 
+        for(let index = 0; index < myArray.length; index++){
+            console.log(myArray[index].name);
+        }
+        */
+
+        /* // loop type 2 - foreach
+        myArray.forEach(person => {
+            console.log(person.name);
+        });
+        */
+
+        /* // loop type 3 - foreach(classic)
+        myArray.forEach(function(person){
+            console.log(person.name);
+        })
+        */
+
+        /* // loop type 4 - for in (modern)
+        for (const index in myArray) {
+            console.log(myArray[index].name);
+        }
+        */
+
+        /* // loop type 5 - for of
+        for (const person of myArray) {
+            console.log(person.name);
+        }
+        */
+
+        /* // loop type 6 - while classic
+        let index = 0;
+        while(index < myArray.length){
+            console.log(myArray[index].name);
+            index++;
+        }
+        */
+
+        // ternary operator
+        let arrayEmpty = (myArray.length > 0) ? false : true;
+
+        // "exactly equal" === checks type and value, == only checks value
+        if(myArray[0].age === 25){
+            console.log("First element is Tom");
+        }
+
+        // associative arrays create this key/value pair association but there is no iterator
+        // which means you can't loop through them
+        let myAssociateArray = [];
+
+        myAssociateArray["Name"]="Tom";
+        myAssociateArray["Age"]= 30;
+        myAssociateArray["StudentNum"]= 30059126;
+
+        console.log(myAssociateArray);
+        console.log(myAssociateArray["Name"]);
+        
+        let myFavouriteThingsList = [
+            "one",
+            "two",
+            "three"
+        ];
+
+        // "hook into" a ul that is empty, that has an id of myFavouriteThings
+        let myFavouritesList = document.getElementById("myFavouriteThings");
+
+        /*
+        myFavouriteThingsList.forEach(thing => {
+            let newItem = document.createElement("li");
+            newItem.textContent = thing;
+            myFavouritesList.appendChild(newItem);
+        });
+        */
+
+        for (let index = 0; index < myFavouriteThingsList.length; index++) {
+            let newItem = document.createElement("li");
+            newItem.textContent = myFavouriteThingsList[index];
+            myFavouritesList.appendChild(newItem);            
+        }
+
+        
+        console.log(myFavouritesList);
+
     }
 
     function Start() {
